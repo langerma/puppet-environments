58477eacf95280690d0a8fa92515fa868da28099
Markus Langer <langer.markus@gmail.com>
2017-01-16 19:06:03 +0100

    added ssh auth-key stuff

--- site.pp
+++ site.pp
@@ -5,4 +5,10 @@ filebucket {
     path =>  false,
 }
 
-hiera_include('classes')
+node default {
+  hiera_include('classes')
+  $ssh_authorized_keys = hiera_hash('ssh_authorized_keys', undef)
+  if ($ssh_authorized_keys != undef) {
+    create_resources('ssh_authorized_key', $ssh_authorized_keys)
+  }
+}
